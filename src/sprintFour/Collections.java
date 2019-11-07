40577f1cf8db928ea3431d93d93dc83c5969e86f
almas <mendygax@gmail.com>
2019-11-06 20:43:56 -0800

    Create Collections.java

---
+++ src/sprintFour/Collections.java
@@ -0,0 +1,19 @@
+package sprintFour;
+
+//Write a Java program to test if a map contains a mapping for the specified key.
+//        Write a Java program to copy all of the mappings from the specified map to another map.
+//        Remove a value from HashMap based on key
+//        Part 2 Sets:
+//        Initiate a Set. Using For loop add integers from 1 to 10
+//        Remove all odd numbers from sets.
+//        Try to create a set with duplicate values
+//        Part 3 Lists:
+//        Initiate an Arraylist. Using For loop add integers from 1 to 10
+//        Create an ArrayList [1,2,3,6,7,3,2,1]. Remove all 2 from Arraylist
+//        Remove all odd numbers;
+
+public class Collections {
+    public static void main(String[] args){
+
+    }
+}
