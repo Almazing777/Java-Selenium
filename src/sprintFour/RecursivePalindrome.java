2da420ca29a975713557fe8cc87c757e4ed8f1b9
almas <mendygax@gmail.com>
2019-11-08 10:07:48 -0800

    updated recursive exercises
    palindrome and fizzbuzz

--- src/sprintFour/RecursivePalindrome.java
+++ src/sprintFour/RecursivePalindrome.java
@@ -2,19 +2,28 @@ package sprintFour;
 
 class RecursivePalindrome {
     static boolean isPalindrome(String str) {
-        int input = 0, reverse = str.length() - 1;
-        while (input < reverse) {
-            if (str.charAt(input) != str.charAt(reverse)) return false;
-            input++;
-            reverse--;
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        if(str.length() < 2){
+            System.out.println("string is a Palindrome");
+            return true;
         }
-        return true;
+
+        if(str.charAt(0) == str.charAt(str.length() - 1)){
+            return isPalindrome(str.substring(1, str.length() - 1));
+        }
+        System.out.println("string is not a Palindrome");
+        return false;
     }
 
     public static void main(String[] args){
+<<<<<<< Updated upstream
         String str = "was it a Cat I sAW";
         str = str.replaceAll("[^a-zA-Z]+", "").toLowerCase();
         if(isPalindrome(str)) System.out.println("yes");
         else System.out.println("no");
+=======
+        isPalindrome("was it a cat i saw");
+>>>>>>> Stashed changes
     }
 }
